aad78d5363c88c161e5c80d022a65d19e94d6212
PEYRON Hugo <hupeyron@gmail.com>
2021-02-26 14:18:14 +0100

    Updating CouponSection to work well on iPhone devices

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { BarCodePage, PopUp } from 'sics'
+import { CouponSection, PopUp } from 'sics'
 import 'sics/dist/index.css'
 
 const App = () => {
+    const [items, setItems] = useState(null)
     useEffect(() => {
-        const items = {
+        const itemsS = {
             'cc_62ec294d-5fa8-454d-ac78-1e83cca06c93': {
                 id: 'cc_62ec294d-5fa8-454d-ac78-1e83cca06c93',
                 product_name: 'Mozzarella',
@@ -13,7 +14,7 @@ const App = () => {
                 brand_name: 'Galbani',
                 image_src:
                     'https://cdn.monoprix.fr/cdn-cgi/image/width=580,quality=75,format=auto,metadata=none/assets/images/grocery/3032567/580x580.jpg',
-                coupon_value: 50,
+                coupon_val: 50,
                 token:
                     'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiZGF0YSJ9.KAxu7OyOPNANLNQBl0mq2Un_0QY3dYw3H4yV2CYzZDM'
             },
@@ -24,25 +25,26 @@ const App = () => {
                 brand_name: 'Monoprix',
                 image_src:
                     'https://cdn.monoprix.fr/cdn-cgi/image/width=580,quality=75,format=auto,metadata=none/assets/images/grocery/2223027/580x580.jpg',
-                coupon_value: 50,
+                coupon_val: 50,
                 token:
                     'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiZGF0YSJ9.KAxu7OyOPNANLNQBl0mq2Un_0QY3dYw3H4yV2CYzZDM'
             }
         }
 
+        setItems(itemsS)
+
         window.localStorage.setItem('sics-items', JSON.stringify(items))
     }, [])
+
+    if (!items) return null
 
     return (
         <React.Fragment>
             <PopUp />
-            <BarCodePage
-                onReceiptNumberValidated={() =>
-                    console.log('receipt validated')
-                }
-                onReceiptNumberInvalidated={() =>
-                    console.log('receipt invalidated')
-                }
+            <CouponSection
+                onButtonClicked={() => console.log('button was clicked')}
+                onToggle={(test) => console.log('status changed')}
+                coupon={Object.values(items)[0]}
             />
         </React.Fragment>
     )
--- src/components/page/product/coupon/section/CouponSection.js
+++ src/components/page/product/coupon/section/CouponSection.js
@@ -12,75 +12,76 @@ import Button from '../../../../generic/button/Button'
 import Toggle from '../../../../generic/toggle/Toggle'
 
 class CouponSection extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      actif: true
+    constructor(props) {
+        super(props)
+        console.log('test proops')
+        console.log(props)
+        this.state = {
+            actif: true
+        }
+        this.componentDidMount = this.componentDidMount.bind(this)
+        this.couponActivationChange = this.couponActivationChange.bind(this)
     }
-    this.componentDidMount = this.componentDidMount.bind(this)
-    this.couponActivationChange = this.couponActivationChange.bind(this)
-  }
 
-  componentDidMount() {
-    const { coupon } = this.props
+    componentDidMount() {
+        const { coupon } = this.props
 
-    storage.getCouponsFromStorage()
+        storage.getCouponsFromStorage()
 
-    storage.addCouponInStorage(coupon.id, coupon)
-  }
-
-  couponActivationChange(status) {
-    const { coupon } = this.props
-
-    this.setState({ actif: status })
-    this.props.onToggle(status)
-
-    if (status) {
-      storage.addCouponInStorage(coupon.id, coupon)
-    } else {
-      storage.removeCouponFromStorage(coupon.id)
+        storage.addCouponInStorage(coupon.id, coupon)
     }
-  }
 
-  render() {
-    const { actif } = this.state
-    const { coupon } = this.props
-    const somme = coupon.coupon_val / 100
+    couponActivationChange(status) {
+        const { coupon } = this.props
 
-    return (
-      <div className={styles.container}>
-        <div className={styles.section}>
-          <div className={styles.text}>
-            <p>
-              Remise de {somme}€ pour l'achat de ce produit. Pour bénéficier,
-              activez le coupon ci-contre, puis rendez-vous sur "Mes avantages"
-              après votre passage en caisse.
-            </p>
-          </div>
-          <div className={styles.toggleZone}>
-            <Toggle
-              defaultChecked={this.state.actif}
-              onStatusChanged={this.couponActivationChange}
-            />
-            <span>Coupon {somme}€</span>
-          </div>
-        </div>
-        {actif && (
-          <div>
-            <Button onClick={this.props.onButtonClicked}>
-              Me faire rembourser {somme}€
-            </Button>
-          </div>
-        )}
-      </div>
-    )
-  }
+        this.setState({ actif: status })
+        this.props.onToggle(status)
+
+        if (status) {
+            storage.addCouponInStorage(coupon.id, coupon)
+        } else {
+            storage.removeCouponFromStorage(coupon.id)
+        }
+    }
+
+    render() {
+        const { actif } = this.state
+        const { coupon } = this.props
+        const somme = coupon.coupon_val / 100
+
+        return (
+            <div className={styles.container}>
+                <div className={styles.section}>
+                    <div className={styles.text}>
+                        <p>
+                            Remise de {somme.toPrecision(2)}€ pour l'achat de ce
+                            produit. Pour bénéficier, activez le coupon
+                            ci-contre, puis rendez-vous sur "Mes avantages"
+                            après votre passage en caisse.
+                        </p>
+                    </div>
+                    <div className={styles.toggleZone}>
+                        <Toggle
+                            defaultChecked={this.state.actif}
+                            onStatusChanged={this.couponActivationChange}
+                        />
+                        <span>Coupon {somme.toPrecision(2)}€</span>
+                    </div>
+                </div>
+                {actif ? (
+                    <Button onClick={this.props.onButtonClicked}>
+                        Me faire rembourser {somme.toPrecision(2)}€
+                    </Button>
+                ) : null}
+            </div>
+        )
+    }
 }
 
 CouponSection.propTypes = {
-  onToggle: PropTypes.func.isRequired,
-  onButtonClicked: PropTypes.func.isRequired,
-  coupon: PropTypes.object.isRequired
+    onToggle: PropTypes.func.isRequired,
+    onButtonClicked: PropTypes.func.isRequired,
+    coupon: PropTypes.object.isRequired
 }
 
 export default CouponSection
